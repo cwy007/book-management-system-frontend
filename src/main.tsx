8232dc95b017468b2d0e58d2757a68d2b2c0fef3
chanweiyan <chanweiyan007@gmail.com>
2024-10-09 18:28:58 +0800

    chore: rm files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login/index.tsx';
-import Register from './pages/Register/index.tsx';
-import BookManage from './pages/BookManage/index.tsx';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BookManage from './pages/BookManage';
 
 const routes: RouteObject[] = [
   {
